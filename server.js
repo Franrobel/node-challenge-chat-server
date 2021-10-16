f7aeffe441db1a20b21466b8c6f472b4d0e7a9d1
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-10-16 13:29:45 +0200

    until level 4

--- server.js
+++ server.js
@@ -5,12 +5,19 @@ const { from } = require("responselike");
 const app = express();
 app.use(express.json())
 
-app.use(cors());
+// app.use(cors());
+// const myLogger = (req, res, next) => {
+//   const visitTime = new Date();
+//   console.log({"visited: " + req.url, "visitTime: " visitTime.toLocaleString()});
+//   next();
+// };
+// app.use(myLogger);
 
 const welcomeMessage = {
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
+  timeSent: new Date()
 };
 
 //This array is our "data store".
@@ -24,17 +31,34 @@ app.get("/", function (request, response) {
 app.get("/messages", (req,res)=>{
   res.send(messages)
 })
-app.get("/messages/:id", (req, res) =>{
+app.get("/messages/id/:id", (req, res) =>{
   const id = parseInt(req.params.id)
- const message = messages.find(mes => mes.id === id)
+  const message = messages.find(mes => mes.id === id)
   res.send(message)
+  // if si devuelve mensaje y sino error
+})
+app.get("/messages/latest", (req,res)=>{
+const last10Message = messages.slice(-10)
+
+res.status(200).send(last10Message)
 })
 
 app.post("/messages", (req,res)=>{
+  const from = req.body.from;
+  const text = req.body.text;
+  
+  if (!text && !from){
+    res.status(400).send({error: "from and text are missing"})
+  } else if (!text && from){
+    res.status(400).send({error: "There is no text in your message"})
+  } else if (text && !from){
+    res.status(400).send({error: "There is no name in from"})
+  }
   const message = {
     id: messages.length,
     from: req.body.from,
-    text: req.body.text
+    text: req.body.text,
+    timeSent: new Date()
    }
    messages.push(message)
   res.send("message added")
@@ -42,10 +66,21 @@ app.post("/messages", (req,res)=>{
 
 app.delete("/messages/:id", (req,res)=>{
   const id = parseInt(req.params.id)
+  
 const index = messages.findIndex(mes => mes.id === id)
 messages.splice(index, 1)
 res.send({from: "deleted"})
 })
+app.get("/messages/search/:text", (req,res) =>{
+  const text = req.params.text.toLowerCase();
+  const findMessage = messages.filter((mes) => mes.text.toLowerCase().includes(text))
+  if (findMessage.length > 0) {
+    return res.status(200).send(findMessage)
+  } else { 
+    return res.status(400).send("message not found")}
+  }
+)
+///messages/search?text=express
 app.listen(3000, () => {
    console.log("Listening on port 3000")
   });
