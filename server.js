a46ba81447699c8cf388a42b14a323091195477f
Francisco Rodriguez Belluati <franrobelluati@gmail.com>
2021-10-18 16:55:18 +0200

    until put method

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 app.use(express.json())
 
 // app.use(cors());
-// const myLogger = (req, res, next) => {
-//   const visitTime = new Date();
-//   console.log({"visited: " + req.url, "visitTime: " visitTime.toLocaleString()});
-//   next();
-// };
-// app.use(myLogger);
+ const myLogger = (req, res, next) => {
+  const visitTime = new Date();
+  console.log({"visited": req.url, "visitTime": visitTime.toLocaleString()});
+  next();
+};
+app.use(myLogger);
 
 const welcomeMessage = {
   id: 0,
@@ -47,12 +47,12 @@ app.post("/messages", (req,res)=>{
   const from = req.body.from;
   const text = req.body.text;
   
-  if (!text && !from){
-    res.status(400).send({error: "from and text are missing"})
-  } else if (!text && from){
+  if (text == "" && from == ""){
+     res.status(400).send({error: "from and text are missing"})
+  } else if (text == "" && from){
     res.status(400).send({error: "There is no text in your message"})
-  } else if (text && !from){
-    res.status(400).send({error: "There is no name in from"})
+  } else if (text && from == ""){
+    res.status(400).send({error: "You forgot to say who is sending the message"})
   }
   const message = {
     id: messages.length,
@@ -66,14 +66,37 @@ app.post("/messages", (req,res)=>{
 
 app.delete("/messages/:id", (req,res)=>{
   const id = parseInt(req.params.id)
-  
+  console.log("id: ", id);
 const index = messages.findIndex(mes => mes.id === id)
 messages.splice(index, 1)
-res.send({from: "deleted"})
+res.send(`id ${id} was deleted`)
 })
+app.put("/messages/:id", (req, res)=>{
+  const messageId = parseInt(req.params.id);
+  let updatedMessage = req.body;
+ 
+  let message = messages.find((message) => message.id === messageId);
+  if (!message) {
+    res.status(404).send("This message does not exist");
+    return;
+  } else if (updatedMessage.text == null || updatedMessage.from == null){
+    res.status(400).send("Some fields are empty");
+    return
+ } 
+ console.log(updatedMessage);
+
+   message.from = updatedMessage.from;
+   message.text = updatedMessage.text;
+   timeSent = message.timeSent
+   res.status(201).send(updatedMessage);
+   
+});
+
+
+
 app.get("/messages/search/:text", (req,res) =>{
   const text = req.params.text.toLowerCase();
-  const findMessage = messages.filter((mes) => mes.text.toLowerCase().includes(text))
+  const findMessage = messages.filter((mes) => mes.text.toLowerCase().includes(text) || mes.from.toLowerCase().includes(text))
   if (findMessage.length > 0) {
     return res.status(200).send(findMessage)
   } else { 
